568c9efccc1f6d4aa89f3edd2f2c7a8439f67706
Tiziano Preisig <tiziano.preisig@hispeed.ch>
2025-01-16 22:46:09 +0100

    Middleware added and server file modified

--- middleware/saveToFile.js
+++ middleware/saveToFile.js
@@ -3,7 +3,16 @@ const fs = require('fs');
 const fsPromises = require('fs').promises;
 const path = require('path');
 
-const saveToFile = async(word, logDetails) => {}
-
+const saveToFile = async (word) => {
+    const logWord = `${format(new Date(), 'yyyy MMM do\t HH:mm:ss')}\t${word}\n`;
+    try {
+        if(!fs.existsSync(path.join(__dirname, '..', 'logs'))){
+            await fsPromises.mkdir(path.join(__dirname, '..', 'logs'));
+        };
+        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', 'loggedWords.txt'), logWord);
+    } catch(error){
+        console.log(error);
+    }
+}
 
 module.exports = saveToFile;
