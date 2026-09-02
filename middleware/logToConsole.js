71b199ada91e87e0cb7a35df1196ffca1d134349
Tiziano Preisig <tiziano.preisig@hispeed.ch>
2026-09-02 22:02:54 +0200

    added code

--- middleware/logToConsole.js
+++
@@ -1,8 +0,0 @@
-const { format } = require('date-fns');
-
-const logToConsole = (message) => {
-    const dateTime = `${format(new Date(), 'yyyy MM dd\tHH:mm:ss')}`;
-    console.log(`${dateTime}\t${message}`);
-}
-
-module.exports = logToConsole;
